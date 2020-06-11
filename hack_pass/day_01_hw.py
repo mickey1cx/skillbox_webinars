2e2604e29e94ebc8e15b52de673e3df1e4dd3745
mickey1cx <mickey1cx@gmail.com>
2020-06-11 22:12:31 +0300

    day 01 results

--- hack_pass/day_01_hw.py
+++ hack_pass/day_01_hw.py
@@ -26,3 +26,10 @@ for site in sites:
         result[r.status_code] = result.setdefault(r.status_code, 0) + 1
         times[r.status_code] = times.setdefault(r.status_code, 0) + delta_time
     print(site, result, times)
+
+# results:
+# https://google.com {200: 100} {200: 32.36373782157898}
+# https://yandex.ru {200: 100} {200: 32.43997502326965}
+# https://mail.ru {200: 100} {200: 16.887038946151733}
+# https://yahoo.com {200: 100} {200: 179.64861798286438}
+# https://msn.com {200: 100} {200: 106.89739465713501}
